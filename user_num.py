b89943b829df845ef67edd151115a4a96e2b33cd
Humais55 <mohammadhumais6@gmail.com>
2023-06-04 19:19:42 +0500

    First Upload

---
+++ user_num.py
@@ -0,0 +1,22 @@
+#Write a program to enter the numbers till the user wants and at the end
+# it should display the maximum and minimum number entered.
+
+numbers = []  # List to store the entered numbers
+
+while True:
+    num = input("\nEnter a number (or 'q' to quit): ")
+    if num.lower() == 'q':
+        break
+    try:
+        num = int(num)
+        numbers.append(num)
+    except ValueError:
+        print("\nInvalid input. Please enter a number or 'q' to quit.")
+
+if numbers:
+    maximum = max(numbers)
+    minimum = min(numbers)
+    print(f"\nMaximum number entered: {maximum}")
+    print(f"\nMinimum number entered: {minimum}")
+else:
+    print("\nNo numbers were entered.")
